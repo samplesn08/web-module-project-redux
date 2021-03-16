45a403ed9526942285024418cf853ccc5260f91f
samplesn08 <samplesn08@gmail.com>
2021-03-16 15:18:12 -0400

    added redux actions and reducers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Header = props => {
+const Header = () => {
   return (
     <>
       <figure className="image is-128x128">
-        <img src={props.car.image} alt={props.car.name} />
+        <img src={this.props.car.image} alt={this.props.car.name} />
       </figure>
-      <h2>{props.car.name}</h2>
-      <p>Amount: ${props.car.price}</p>
+      <h2>{this.props.car.name}</h2>
+      <p>Amount: ${this.props.car.price}</p>
     </>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    car: state.car,
+  }
+}
+
+export default connect(mapStateToProps, {})(Header);
